5be1ad75277eb9f990b04540a58965e3eefa675c
CaoYuYang <caoyuyanghome@163.com>
2025-10-30 14:04:17 +0800

    2025-10-30 01

---
+++ com/learn/methodDem/pointer.java
@@ -0,0 +1,32 @@
+package com.learn.methodDem;
+
+public class pointer {
+    /*
+     * 给定一个递增有序数组，去除其中重复元素
+     *
+     * */
+    public static void main(String[] args) {
+        // 有序数组
+        int[] arr = {1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+
+        int left = 0;
+        int right;
+
+        for (int i = 0; i < arr.length; i++) {
+            right = i;
+            if (arr[right] != arr[left]){
+                left++;
+                arr[left] = arr[right];
+            }
+        }
+
+        for (int j : arr) {
+            System.out.println(j + " ");
+        }
+
+
+    }
+
+
+}
+
