784dbaf3c2f55a6ea17835e4441903d1e4d88f2a
CaoYuYang <caoyuyanghome@163.com>
2025-11-03 14:30:01 +0800

    2025-11-03-04

--- Go/Study/main.go
+++ Go/Study/main.go
@@ -8,11 +8,13 @@ func main() {
 	var name = "yuyang"
 	fmt.Println(name)
 
-	var input string
-	fmt.Print("在次输入：")
-	fmt.Scanln(&input)
-	fmt.Print(input)
+	//var input string
+	//fmt.Print("在次输入：")
+	//fmt.Scanln(&input)
+	//fmt.Print(input)
 
-	fmt.Scan(&input)
-	fmt.Println(input)
+	var a = 'a'
+
+	fmt.Printf("%c %d\n", a, a)
+
 }
