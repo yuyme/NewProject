795b9cbd64a31ab265c0c77726769768d370adac
CaoYuYang <caoyuyanghome@163.com>
2025-11-04 15:00:08 +0800

    2025-11-04 01

--- Go/Study/main.go
+++ Go/Study/main.go
@@ -12,9 +12,18 @@ func main() {
 	//fmt.Print("在次输入：")
 	//fmt.Scanln(&input)
 	//fmt.Print(input)
-
+	var b uint = 1
+	var c int8 = 1
+	var d int16 = 1
+	var e int32 = 1
+	var f int64 = 1
 	var a = 'a'
 
 	fmt.Printf("%c %d\n", a, a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 
 }
--- Java/learn/DataType/arrDemo.java
+++ Java/learn/DataType/arrDemo.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class arrDemo {
     public static void main(String[] args) {
-        int[] arr = {33, 5, 22, 44, 55, 33, 47, 34 , 99, 5};
+        int[] arr = {0,1,2,2,3,0,4,2};
 
 
+        // isExce(arr);
+
+        int[] arrs = numArr(arr, 2);
+        for (int i : arrs) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void isExce(int[] arr) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入任意一个两位整数：");
         int num = sc.nextInt();
@@ -23,5 +32,26 @@ public class arrDemo {
         }
         if (flag) System.out.println(index);
         else System.out.println("该数据不存在");
+    }
+
+    private static int[] numArr(int[] arr, int val) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val){
+                arr[i] = -1;
+                count++;
+            }
+        }
+
+        int[] newArr = new int[arr.length - count];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != -1){
+                newArr[index] = arr[i];
+                index++;
+            }
+        }
+
+        return newArr;
     }
 }
