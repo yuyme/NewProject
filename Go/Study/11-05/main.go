1c3f31327f3ae59fed4d3734810a3f68945202d1
CaoYuYang <caoyuyanghome@163.com>
2025-11-05 15:06:17 +0800

    2025-11-03-04

---
+++ Go/Study/11-05/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	price := 99
+
+	fmt.Println(price)
+
+	//成绩判断
+	//fmt.Print("请输入你的成绩：")
+	//var score int
+	//fmt.Scan(&score)
+	score := 72
+
+	if score >= 90 {
+		fmt.Println("优秀")
+	} else if score >= 80 {
+		fmt.Println("良好")
+	} else if score >= 70 {
+		fmt.Println("一般")
+	} else if score >= 60 {
+		fmt.Println("及格")
+	} else {
+		fmt.Println("较差 不及格")
+	}
+
+	score2 := 92
+
+	switch {
+	case score2 >= 90:
+		fmt.Println("优秀")
+	case score2 >= 80:
+		fmt.Println("良好")
+	case score2 >= 70:
+		fmt.Println("一般")
+	case score2 >= 60:
+		fmt.Println("及格")
+	default:
+		fmt.Println("较差 不及格")
+	}
+
+}
